8877106877ebffae9cdf9a9e4c85ac17c4d14ba9
David E. Craig <decyahoo-github@yahoo.com>
2021-10-20 13:53:33 -0400

    Adding GitIgnore and icon

--- OfficeWebAddInVerifier/Properties/AssemblyInfo.cs
+++ OfficeWebAddInVerifier/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OfficeWebAddInVerifier")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Office Web Add-In Verifier")]
+[assembly: AssemblyDescription("Verifies a web add-in manifest, the web server install and OWA connection")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OfficeWebAddInVerifier")]
+[assembly: AssemblyCompany("Microsoft Customer Engineering")]
+[assembly: AssemblyProduct("Office Web Add-In Verifier")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
